d7d842d21e031f86e62a778fb833fa203c14d5b0
Sacerdoce Lau <116038705+Maelau@users.noreply.github.com>
2025-03-01 00:45:40 +0000

    Update script.js
    Events on searchbar and displaying console results.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function addMarker(lat, lng) {
     L.marker([lat, lng]).addTo(map);
 }
+
+document.getElementById('search').addEventListener('input', function()
+{
+    const query = this.value.toLowerCase();
+    const results = places.filter(place => place.name.toLowerCase().includes(query));
+
+    if (results.length > 0) {
+        const firstResult = results[0];
+        map.setView([firstResult.lat, firstResult.lng], 13);
+        addMarker(firstResult.lat, firstResult.lng);
+    }
+}
+);
